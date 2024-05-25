c719b76dee8b0adb6ba4b28b33944c8090470ad3
AbdelmonemMarei <89366095+AbdelmonemMarei@users.noreply.github.com>
2024-05-26 00:29:29 +0300

    Update main.js
    add display none style to forward card

--- Newbie/interactive-rating-component-main/js/main.js
+++ Newbie/interactive-rating-component-main/js/main.js
@@ -1,20 +1,20 @@
 // Get elements
 const ratingBtns = document.querySelectorAll(".rating");
 const submitBtn = document.querySelector(".submit");
-let selected =" "
 ratingBtns.forEach(el => el.addEventListener("click",
 (e)=>{
     ratingBtns.forEach(el => {
         el.classList.remove("active")
     });
     e.target.classList.add("active");
-    selected = el.textContent;
+    let selected = el.textContent;
     submitBtn.addEventListener("click",(e)=>{
         if(!selected){
             e.preventDefault;
         }
         document.querySelector(".selected").textContent =`You selected ${selected} out of 5`;
         document.querySelector(".card-backward").style.display="flex";
+        document.querySelector(".card-forward").style.display="none";
     })
 }
 ))
